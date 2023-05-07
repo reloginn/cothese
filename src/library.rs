5cb95b7818df896bad8f589f3326e2fb8359f7c6
reloginn <nikita.malina23@gmail.com>
2023-05-07 13:00:28 +0600

    minor changes

--- src/library.rs
+++ src/library.rs
@@ -1,12 +1,12 @@
-use walkdir::WalkDir;
+use image::{imageops::resize, imageops::FilterType::Triangle, DynamicImage, ImageBuffer};
 use std::path::{Path, PathBuf};
-use image::{DynamicImage, imageops::resize, imageops::FilterType::Triangle};
-use webp::{WebPMemory, Encoder};
 use std::time::Instant;
+use walkdir::WalkDir;
+use webp::{Encoder, WebPMemory};
 #[derive(Debug)]
 pub struct Compressor<'a> {
-    pub input_directory: Box<&'a Path>,
-    pub output_directory: Box<&'a Path>,
+    pub input_directory: &'a Path,
+    pub output_directory: &'a Path,
     iterator: u16,
 }
 
@@ -14,25 +14,35 @@ impl<'a> Compressor<'a> {
     pub fn new(input_directory: &'static str, output_directory: &'static str) -> Self {
         let input_path = Path::new(input_directory);
         if !input_path.exists() {
-            panic!("Указаной входной директории не существует, укажите другую, либо создайте эту: {:?}", input_path) 
+            panic!("Указаной входной директории не существует, укажите другую, либо создайте эту: {:?}", input_path)
         }
         let output_path = Path::new(output_directory);
         if !output_path.exists() {
             panic!("Указанной выходной директории не существует, укажите другую, либо создайте эту: {:?}", output_path)
         }
-        Self { input_directory: Box::new(input_path), output_directory: Box::new(output_path), iterator: 0 }
+        Self {
+            input_directory: input_path,
+            output_directory: output_path,
+            iterator: 0,
+        }
     }
-    pub async fn compress_of_images(mut self) {
-        let file_paths = &self.get_all_of_files_in_directory().await;
+    pub fn compress_of_images(mut self) {
+        let file_paths = &self.get_all_of_files_in_directory();
         for file in file_paths.iter() {
             let path = Path::new(file);
             if path.is_file() {
                 match path.extension().unwrap_or_default().to_str().unwrap() {
                     "png" | "jpg" | "jpeg" => {
                         self.iterator += 1;
-                        convert(Box::new(path), Box::new(&self.output_directory.join(format!("{}", self.iterator)).with_extension("webp"))).await;
+                        convert(
+                            path,
+                            &self
+                                .output_directory
+                                .join(format!("{}", self.iterator))
+                                .with_extension("webp"),
+                        );
                     }
-                    _ => ()
+                    _ => (),
                 }
             }
         }
@@ -42,10 +52,10 @@ impl<'a> Compressor<'a> {
             println!("Успешно конвертировано {} картинок", self.iterator);
         }
     }
-    async fn get_all_of_files_in_directory(&self) -> Vec<PathBuf> {
+    fn get_all_of_files_in_directory(&self) -> Vec<PathBuf> {
         let mut file_paths: Vec<PathBuf> = Vec::new();
 
-        for entry in WalkDir::new(&self.input_directory.as_ref())
+        for entry in WalkDir::new(&self.input_directory)
             .max_depth(1)
             .into_iter()
             .filter_map(|e| e.ok())
@@ -59,10 +69,10 @@ impl<'a> Compressor<'a> {
     }
 }
 
-async fn convert(input_path: Box<&Path>, output_path: Box<&PathBuf>) {
+fn convert(input_path: &Path, output_path: &PathBuf) {
     let start = Instant::now();
     println!("Начинаю конвертировать файл {:?}...", &input_path);
-    let img = image::open(input_path.as_ref()).unwrap();
+    let img = image::open(input_path).unwrap();
     let (w, h) = (img.width(), img.height());
     let size_factor = 1.0;
     let img: DynamicImage = image::DynamicImage::ImageRgba8(resize(
@@ -73,6 +83,6 @@ async fn convert(input_path: Box<&Path>, output_path: Box<&PathBuf>) {
     ));
     let encoder: Encoder = Encoder::from_image(&img).unwrap();
     let webp: WebPMemory = encoder.encode(90f32);
-    std::fs::write(&output_path.as_ref(), &*webp).unwrap();
+    std::fs::write(&output_path, &*webp).unwrap();
     println!("Файл успешно конвертирован за {:?}", start.elapsed());
 }
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ mod library;
 #[tokio::main]
 async fn main() {
     let images = Compressor::new("./input_images/", "./output_images/");
-    images.compress_of_images().await;
+    images.compress_of_images();
 }
