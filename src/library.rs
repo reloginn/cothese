64581ebb6c12fa8c585a5ff4fb18cc004754d252
Никита <nikita.malina23@gmail.com>
2023-05-11 21:11:30 +0600

    improve code

--- src/library.rs
+++ src/library.rs
@@ -1,45 +1,74 @@
 use image::{imageops::resize, imageops::FilterType::Triangle, DynamicImage};
-use std::path::{Path, PathBuf};
-use std::time::Instant;
+use std::{
+    fmt::Display,
+    path::{Path, PathBuf},
+    sync::Arc,
+    time::Instant,
+};
 use walkdir::WalkDir;
 use webp::{Encoder, WebPMemory};
 
+#[allow(unused)]
 #[derive(Debug)]
-pub struct Compressor<'a> {
-    pub input_directory: &'a Path,
-    pub output_directory: &'a Path,
-    iterator: u16,
+pub enum SelfErrors<T>
+where
+    T: Display,
+{
+    InvalidDirectory(T),
+    Other(T),
 }
 
-impl<'a> Compressor<'a> {
-    pub fn new(input_directory: &'static str, output_directory: &'static str) -> Self {
-        let input_path = Path::new(input_directory);
-        if !input_path.exists() {
-            panic!("Указаной входной директории не существует, укажите другую, либо создайте эту: {:?}", input_path)
-        }
-        let output_path = Path::new(output_directory);
-        if !output_path.exists() {
-            panic!("Указанной выходной директории не существует, укажите другую, либо создайте эту: {:?}", output_path)
-        }
-        Self {
-            input_directory: input_path,
-            output_directory: output_path,
-            iterator: 0,
+impl<T> Display for SelfErrors<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidDirectory(err) => writeln!(f, "{}", err),
+            Self::Other(err) => writeln!(f, "{}", err),
         }
     }
-    pub fn compress_of_images(mut self) {
+}
+
+#[derive(Debug)]
+pub struct Compressor {
+    input_directory: Arc<PathBuf>,
+    output_directory: Arc<PathBuf>,
+}
+
+impl Compressor {
+    pub fn new(
+        input_directory: &'static str,
+        output_directory: &'static str,
+    ) -> Result<Self, SelfErrors<String>> {
+        let (input_path, output_path) = (
+            PathBuf::from(input_directory),
+            PathBuf::from(output_directory),
+        );
+        if !input_path.exists() || !output_path.exists() {
+            return Err(SelfErrors::InvalidDirectory(String::from(
+                "Одна из директорий невалидна",
+            )));
+        }
+        Ok(Self {
+            input_directory: Arc::new(input_path),
+            output_directory: Arc::new(output_path),
+        })
+    }
+    pub fn compress_of_images(&self) {
         let file_paths = &self.get_all_of_files_in_directory();
+        let mut iter: u16 = 0;
         for file in file_paths.iter() {
             let path = Path::new(file);
             if path.is_file() {
                 match path.extension().unwrap_or_default().to_str().unwrap() {
                     "png" | "jpg" | "jpeg" => {
-                        self.iterator += 1;
+                        iter += 1;
                         convert(
                             path,
                             &self
                                 .output_directory
-                                .join(format!("{}", self.iterator))
+                                .join(format!("{}", iter))
                                 .with_extension("webp"),
                         );
                     }
@@ -47,16 +76,16 @@ impl<'a> Compressor<'a> {
                 }
             }
         }
-        if self.iterator <= 0 {
-            println!("В заданной директории не было никаких картинок формата JPG, PNG, JPEG, ничего не затронуто");
+        if iter <= 0 {
+            println!("В заданной директории не было JPEG, PNG картинок, ничего не затронуто");
         } else {
-            println!("Успешно конвертировано {} картинок", self.iterator);
+            println!("Успешно конвертировано {} картинок", iter)
         }
     }
     fn get_all_of_files_in_directory(&self) -> Vec<PathBuf> {
         let mut file_paths: Vec<PathBuf> = Vec::new();
 
-        for entry in WalkDir::new(&self.input_directory)
+        for entry in WalkDir::new(&*self.input_directory)
             .max_depth(1)
             .into_iter()
             .filter_map(|e| e.ok())
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use self::library::Compressor;
 mod library;
 
 fn main() {
-    let images = Compressor::new("./input_images/", "./output_images/");
+    let images = Compressor::new("./input_images/", "./output_images/").unwrap();
     images.compress_of_images();
 }
