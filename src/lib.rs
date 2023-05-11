062a34e140b3f8db2d076a2fe7c02dc252afc56a
Никита <nikita.malina23@gmail.com>
2023-05-12 00:03:59 +0600

    improve code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
+#![allow(unused)]
+
 pub use self::compressor::Compressor;
+use rand::Rng;
+
+const ASCII: &str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+const NUMBERS: &str = "0123456789";
+const LEN: u8 = 16;
 
 mod kinds {
     pub mod png;
     pub mod webp;
 }
 mod compressor;
+
+fn generate_random_name() -> String {
+    let mut result = String::new();
+    let ascii: Vec<char> = ASCII.chars().collect();
+    let numbers: Vec<char> = NUMBERS.chars().collect();
+    for _ in 0..LEN {
+        let (random_ascii, random_numbers) = (
+            rand::thread_rng().gen_range(0..ascii.len()),
+            rand::thread_rng().gen_range(0..numbers.len()),
+        );
+        result.push(ascii[random_ascii]);
+        result.push(numbers[random_numbers]);
+    }
+    result
+}
