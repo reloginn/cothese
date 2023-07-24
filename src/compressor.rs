1b25a954471774e118fb1d6d13f93a0a6c474934
reloginn <nikita.malina23@gmail.com>
2023-07-24 22:31:57 +0300

    minor changes

--- src/compressor.rs
+++ src/compressor.rs
@@ -1,6 +1,15 @@
-use std::{fs, path::PathBuf, process::exit};
+use std::{fs, path::PathBuf, process::exit, time::Instant};
 
-use crate::{threadpool::ThreadPool, trash::compress_to_webp, consts::{BOLD, RED, DEFAULT}};
+use image::{
+    imageops::{resize, FilterType::Triangle},
+    DynamicImage,
+};
+use webp::{Encoder, WebPMemory};
+
+use crate::{
+    consts::{BLUE, BOLD, DEFAULT, RED},
+    threadpool::ThreadPool,
+};
 
 pub struct Compressor {
     input_dir: PathBuf,
@@ -30,24 +39,70 @@ impl Compressor {
     }
     pub fn run(mut self) {
         let pool = ThreadPool::new(self.threads);
-        let entries = fs::read_dir(self.input_dir).unwrap_or_else(|err| {
-            eprintln!("Error has occured: {}{}{}{}", BOLD, RED, err, DEFAULT);
-            exit(1)
-        });
-        for entry in entries.flatten() {
-            let path = entry.path();
-            if path.is_file() {
-                if let "png" | "jpeg" | "jpg" = path.extension().unwrap().to_str().unwrap() {
-                    let output_dir = self
-                        .output_dir
-                        .join(format!("{}", self.iter))
-                        .with_extension("webp");
-                    let logs = self.logs;
-                    let quality = self.quality;
-                    self.iter += 1;
-                    pool.execute(move || compress_to_webp(path, output_dir, logs, quality))
+        fs::read_dir(self.input_dir)
+            .unwrap_or_else(|err| {
+                eprintln!("Error has occured: {}{}{}{}", BOLD, RED, err, DEFAULT);
+                exit(1)
+            })
+            .flatten()
+            .for_each(|entry| {
+                let path = entry.path();
+                if path.is_file() {
+                    if let "png" | "jpeg" | "jpg" = path.extension().unwrap().to_str().unwrap() {
+                        let output_dir = self
+                            .output_dir
+                            .join(format!("{}", self.iter))
+                            .with_extension("webp");
+                        let logs = self.logs;
+                        let quality = self.quality;
+                        self.iter += 1;
+                        pool.execute(move || compress_to_webp(path, output_dir, logs, quality))
+                    }
                 }
-            }
-        }
+            });
     }
+}
+
+fn compress_to_webp(path: PathBuf, output: PathBuf, logs: bool, quality: f32) {
+    if logs {
+        let start = Instant::now();
+        println!(
+            "{}{}[INFORMATION]{} .png, .jpg, .jpeg => .webp...",
+            BOLD, BLUE, DEFAULT
+        );
+        compress_it(path, output, quality);
+        println!(
+            "{}{}[INFORMATION]{} .png, .jpg, .jpeg => .webp in {:?}",
+            BOLD,
+            BLUE,
+            DEFAULT,
+            start.elapsed()
+        );
+    } else {
+        compress_it(path, output, quality)
+    }
+}
+
+fn compress_it(path: PathBuf, output: PathBuf, quality: f32) {
+    let image = image::open(path).unwrap_or_else(|err| {
+        eprintln!("Error has occured: {}{}{}{}", BOLD, RED, err, DEFAULT);
+        exit(1)
+    });
+    let (width, height) = (image.width(), image.height());
+    const SIZE_FACTOR: f64 = 1.0;
+    let img: DynamicImage = image::DynamicImage::ImageRgba8(resize(
+        &image,
+        (width as f64 * SIZE_FACTOR) as u32,
+        (height as f64 * SIZE_FACTOR) as u32,
+        Triangle,
+    ));
+    let encoder: Encoder = Encoder::from_image(&img).unwrap_or_else(|err| {
+        eprintln!("Error has occured: {}{}{}{}", BOLD, RED, err, DEFAULT);
+        exit(1)
+    });
+    let webp: WebPMemory = encoder.encode(quality);
+    std::fs::write(output, &*webp).unwrap_or_else(|err| {
+        eprintln!("Error has occured: {}{}{}{}", BOLD, RED, err, DEFAULT);
+        exit(1)
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use std::process::exit;
 
 use compressor::Compressor;
 
-use crate::{args_parser::parse_args, consts::{BOLD, RED, DEFAULT, YELLOW}};
+use crate::{
+    args_parser::parse_args,
+    consts::{BOLD, DEFAULT, RED, YELLOW},
+};
 
 mod args_parser;
 mod compressor;
 mod consts;
 mod threadpool;
-mod trash;
 
 fn main() {
     match parse_args() {
         Ok(appargs) => {
             if let Some(quality) = appargs.quality {
                 if !(10.0..=100.0).contains(&quality) {
-                    eprintln!("{}{}Quality cannot be lower than 10.0 and higher than 100.0{}", BOLD, RED, DEFAULT);
+                    eprintln!(
+                        "{}{}Quality cannot be lower than 10.0 and higher than 100.0{}",
+                        BOLD, RED, DEFAULT
+                    );
                     exit(1)
                 }
             }
